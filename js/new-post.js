530571e0942637884261938ae05921b5dc4f53de
jubayerjuhan <davidjuhan23@gmail.com>
2020-12-11 19:45:52 +0600

    Editing The New Post Page

--- js/new-post.js
+++ js/new-post.js
@@ -8,4 +8,22 @@ const API_URL = "http://localhost:3000/api/posts";
 const submitNewPost = () => {
     // HINT: Use FormData to store data to send over
     // HINT: Redirect the user to home page after successful submission
+
+    const input = document.getElementById("form-post-file");
+    const title = document.getElementById("form-post-title").value;
+    const content = document.getElementById("form-post-content").value
+
+
+    let data = new FormData();
+    data.append = ("post_image", input.files[0]);
+    data.append = ("title", title);
+    data.append = ("content", content);
+
+
+    fetch(API_URL, {
+        method: "POST",
+        body: data
+    }).then(() => {
+        console.log("We Are Good");
+    })
 }
